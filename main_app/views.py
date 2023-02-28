de161c49f73907eee5bce1df9929886abf01cc15
EdASalazar <edasalazar123@gmail.com>
2023-02-28 15:49:15 -0800

    Building up websites

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def home(request):
+    return render(request, 'home.html')
+
+def index(request):
+    return HttpResponse("Hello, world. You're at the guitars index.")
+
+def about(request):
+    return render(request, 'about.html')
