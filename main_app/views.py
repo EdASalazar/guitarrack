1a3e986369a315b3934cc6c48994d708dea0fd6f
EdASalazar <edasalazar123@gmail.com>
2023-03-01 13:51:53 -0800

    UpdateView DeleteView

--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Guitar
 
 # Create your views here.
@@ -22,3 +22,12 @@ def guitars_detail(request, guitar_id):
 class GuitarCreate(CreateView):
     model = Guitar
     fields = '__all__'
+
+class GuitarUpdate(UpdateView):
+    model = Guitar
+    fields = ['make', 'color']
+
+class GuitarDelete(DeleteView):
+    model = Guitar
+    success_url = '/cats'
+
